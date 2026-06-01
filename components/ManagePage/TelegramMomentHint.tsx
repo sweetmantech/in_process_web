95f2768aefa95dfacef8915589fdc88dc1e700e5
ziad <tech.eng.me@gmail.com>
2026-06-02 08:32:12 +1000

    fix chat bot handle

--- components/ManagePage/TelegramMomentHint.tsx
+++ components/ManagePage/TelegramMomentHint.tsx
@@ -2,7 +2,7 @@ const TelegramMomentHint = () => (
   <p className="pt-1 text-sm text-grey-moss-300 font-spectral-italic">
     To create moments via Telegram, set your username and start a conversation with{" "}
     <a
-      href="https://t.me/in_process_creator_bot"
+      href="https://t.me/in_process_chat_bot"
       target="_blank"
       rel="noopener noreferrer"
       className="underline"
